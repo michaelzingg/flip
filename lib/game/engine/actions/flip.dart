5d95f0b97fbea2033b29f9f1ed351131035fea6f
Michael Zingg <michael.zingg@gmail.com>
2023-08-09 12:44:40 +0200

    Basic implementation of flip action with first test

--- lib/game/engine/actions/flip.dart
+++ lib/game/engine/actions/flip.dart
@@ -1,5 +1,43 @@
+import 'package:flip_flutter/game/engine/model/die.dart';
+import 'package:flip_flutter/game/engine/model/player.dart';
+import 'package:fpdart/fpdart.dart';
+
 import '../model/game_state.dart';
 
-typedef FlipAction = GameState Function(int dieId, GameState state);
+typedef FlipAction = Either<String, GameState> Function(
+    int dieId, GameState state);
 
-// TODO implement flip() function
+final FlipAction flip = (int dieId, GameState state) {
+  return state.dice
+      .where((element) => element.dieId == dieId)
+      .firstOption
+      .toEither(() => 'Die with id $dieId not found')
+      .filterOrElse((r) => r is PlayerDie, (r) => 'Can only flip player dice')
+      .filterOrElse((r) => r is UnflippedDie, (r) => 'Die already flipped')
+      .map((a) => flipDie(a as UnflippedDie))
+      .map(replaceDie(state))
+      .map(resetRecoverableEyes)
+      .map(changeTurns);
+};
+
+FlippedDie flipDie(UnflippedDie die) {
+  return FlippedDie(die.dieId, die.value, die.player);
+}
+
+final replaceDie = (GameState state) {
+  return (FlippedDie die) => GameState(
+      dice: state.dice.map((e) => e.dieId == die.dieId ? die : e),
+      turn: state.turn,
+      recoverableEyes: state.recoverableEyes);
+};
+
+GameState resetRecoverableEyes(GameState state) {
+  return GameState(dice: state.dice, turn: state.turn, recoverableEyes: 0);
+}
+
+GameState changeTurns(GameState state) {
+  return GameState(
+      dice: state.dice,
+      turn: state.turn == Player.one ? Player.two : Player.one,
+      recoverableEyes: state.recoverableEyes);
+}
---
+++ test/game/engine/actions/flip_test.dart
@@ -0,0 +1,29 @@
+import 'package:flip_flutter/game/engine/actions/create.dart';
+import 'package:flip_flutter/game/engine/actions/flip.dart';
+import 'package:flip_flutter/game/engine/model/die.dart';
+import 'package:flip_flutter/game/engine/model/game_state.dart';
+import 'package:flip_flutter/game/engine/model/player.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+
+void main() {
+  final testDice = create(seed: 77).dice;
+  group('flip', () {
+    test('should return state with flipped die for player one', () {
+      final testState =
+          GameState(dice: testDice, turn: Player.one, recoverableEyes: 0);
+      final result = flip(0, testState);
+      expect(
+          result
+              .getRight()
+              .getOrElse(
+                  () => fail('Expected Either.right, but was Either.left'))
+              .dice
+              .filter((t) => t.dieId == 0)
+              .first,
+          isA<FlippedDie>());
+    });
+
+    // TODO: write more tests
+  });
+}
